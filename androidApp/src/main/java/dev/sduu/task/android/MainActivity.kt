95cadcb7d97f8099a563fe9ad7094b053c59c7f8
Sdu <hey@sduu.dev>
2025-02-05 17:51:41 +0200

    added some code

--- androidApp/src/main/java/dev/sduu/task/android/MainActivity.kt
+++ androidApp/src/main/java/dev/sduu/task/android/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,9 +18,14 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -32,18 +39,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
+import dev.sduu.task.android.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            MyApplicationTheme {
+            AppTheme {
                 TaskContent()
             }
         }
@@ -92,21 +100,45 @@ fun TaskContent(modifier: Modifier = Modifier) {
                     }
                 }
             }
-            Surface(tonalElevation = 1.dp) {
-                Column {
-                    Box {
-                        BasicTextField(
-                            text,
-                            textStyle = LocalTextStyle.current,
-                            onValueChange = { text = it },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp)
-                        )
-                        if (text.isEmpty()) Text(
-                            "What's on your mind?",
-                            modifier = Modifier.padding(16.dp).alpha(0.4f)
-                        )
+            Box(modifier = Modifier.padding(16.dp)) {
+                Surface(
+                    tonalElevation = 1.dp,
+                    modifier = Modifier.clip(RoundedCornerShape(22.dp))
+                ) {
+                    Column {
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            Spacer(Modifier.padding(4.dp))
+                            Box(modifier = Modifier
+                                .weight(1f)
+                                .align(Alignment.CenterVertically)) {
+                                BasicTextField(
+                                    text,
+                                    textStyle = LocalTextStyle.current,
+                                    onValueChange = { text = it },
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(8.dp)
+                                )
+                                if (text.isEmpty()) Text(
+                                    "What's on your mind?",
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(8.dp)
+
+                                        .alpha(0.4f)
+                                )
+                            }
+                            IconButton(
+                                onClick = {},
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            ) {
+                                Icon(
+                                    Icons.Rounded.KeyboardArrowRight,
+                                    contentDescription = "",
+                                    modifier = Modifier.alpha(0.6f)
+                                )
+                            }
+                        }
                     }
                 }
             }
